ec8082b3c32e4234874d82312b2391371dfcd380
phalbanez <pha@virtualsystem.net.br>
2020-02-26 21:05:36 -0300

    Commit inicial

---
+++ src/styles/global.js
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from 'styled-components';
+
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+  
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    background: linear-gradient(-90deg, #7159c1, #ab59c1);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    /* display: flex; */
+    /* align-items: center; */
+    text-decoration: none;
+    transition: opacity 0.2s;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+
+`;
